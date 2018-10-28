01bb4ad613e5e76d6f6afe88ae2bd61f924be551
Rahman <rahmanromli@gmail.com>
2018-10-28 16:58:46 +0800

    Move bouncing ball

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var ctx = canvas.getContext('2d')
 
 var x = canvas.width/2
 var y = canvas.height-30
+var dx = 2		// In physic; Delta X.. same below for Y
+var dy = -2
 var ballRadious=10
 
 function drawBall() {
@@ -15,6 +17,9 @@ function drawBall() {
 
 function draw() {
 	drawBall(draw)
+	x += dx
+	y += dy
+
 	requestAnimationFrame(draw)
 }
 
